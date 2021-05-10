38d58983ebe223a709644ede9b1bdf836da830cd
EugeneJoe <jmuthui70@gmail.com>
2021-05-10 17:53:57 +0000

    Use linked list for instruction_t struct

--- monty.c
+++ monty.c
@@ -1,5 +1,7 @@
 #include "monty.h"
 
+stack_t *head = NULL;
+
 /**
  * main - interpreter for monty byte code
  * @argc: number of arguments to main
@@ -10,24 +12,19 @@
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	stack_t *head = NULL;
-	unsigned int line_no = 0, int_arg = 0, ops = 0, status = 0;
+	unsigned int line_no = 0, int_arg = 0, status = 0;
 	char *line = NULL, *command = NULL, *arg = NULL;
 	size_t size;
-	funcs_t *headptr = NULL;
+	funcs_t *headptr = NULL, *tmp = NULL;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	funcs[0]->opcode = malloc(sizeof(char) * 5);
-	strcpy(funcs[0]->opcode, "push");
-	funcs[0]->f = add_dnodeint;
-	funcs[1]->opcode = malloc(sizeof(char) * 5);
-	strcpy(funcs[1]->opcode, "pall");
-	funcs[1]->f = print_dlistint;
-
+	funcs_init(&headptr);
+	printf("funcs_t initialized\n");
+	tmp = headptr;
 	printf("Opening file...\n");
 	fp = fopen(argv[1], "r");
 	if (fp == NULL)
@@ -39,17 +36,22 @@ int main(int argc, char *argv[])
 	{
 		line_no++;
 		command = strtok(line, " ");
+		arg = strchr(command, '\n');
+		if (arg)
+			*arg = 0;
 		arg = strtok(NULL, " ");
-		printf("Command: %s, argument: %s\n", command, arg);
-		for (ops = 0; ops < 2; ops++)
+		printf("Command: $%s$, Argument: %s\n", command, arg);
+		while (tmp != NULL)
 		{
-	  		if (strcmp(command, funcs[ops]->opcode) == 0)
+	  		if (strcmp(command, tmp->n->opcode) == 0)
 			{
-				int_arg = atoi(arg);
-				funcs[ops]->f(&head, int_arg);
+				if (arg)
+					int_arg = atoi(arg);
+				tmp->n->f(&head, int_arg);
 				status = 1;
 				break;
 			}
+			tmp =  tmp->next;
 		}
 		if (status == 0)
 		{
@@ -57,6 +59,7 @@ int main(int argc, char *argv[])
 line_no, command);
 			exit(EXIT_FAILURE);
 		}
+		status = 0;
 	}
 	if (!feof(fp))
 	{
