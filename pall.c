904e5213e9ea9e67a8c0ed61240322eece8af661
EugeneJoe <jmuthui70@gmail.com>
2021-05-10 17:55:00 +0000

    print linked list

--- pall.c
+++ pall.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * print_dlistint - prints all elements in a list
  * @h: pointer to the head node
@@ -6,13 +7,13 @@
  * Return: returns the number of elements in a node
  */
 
-void print_dlistint(stack_t **h, unsigned int line_no)
+void print_dlistint(stack_t **h, unsigned int line_number)
 {
-
 	stack_t *current = *h;
 	size_t count = 0;
-	(void)line_no;
+	(void)line_number;
 
+	printf("Printing list...\n");
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
