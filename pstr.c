1ac4b81b04ab4d66e29499ec2bf40a169134eb81
EugeneJoe <jmuthui70@gmail.com>
2021-05-12 20:08:36 +0000

    Print string using elements of stack

---
+++ pstr.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * pstr_stack - print a string using stack elements from the top
+ * @head: double pointer to head node of stack
+ * @line_number: line number of instruction in the file being read
+ *
+ * Return: no return value (void)
+ */
+void pstr_stack(stack_t **head, unsigned int line_number)
+{
+	stack_t *temp = *head;
+	(void)line_number;
+
+	if (temp == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	while (temp)
+	{
+		if (temp->n > 0 && temp->n < 178)
+			printf("%c", temp->n);
+		else
+			break;
+		temp = temp->next;
+	}
+	printf("\n");
+}
