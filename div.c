4a6e5df8c1be102d36227e8dad1945bb026f74d0
EugeneJoe <jmuthui70@gmail.com>
2021-05-12 18:54:43 +0000

    Multiplies top two stack elements

---
+++ div.c
@@ -0,0 +1,42 @@
+#include "monty.h"
+
+/**
+ * divide - Divide 2nd top stack element by the top element
+ * @head: double pointer to stack head node
+ * @line_number: line number of instruction in the file
+ *
+ * Return: no return value (void)
+ */
+void divide(stack_t **head, unsigned int line_number)
+{
+	stack_t *current = *head;
+	int res = 0, count = 0;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n",
+			line_number);
+		int_arg = -1;
+		return;
+	}
+	current = *head;
+	if (current->n != 0)
+	{
+		res = current->next->n / current->n;
+	}
+	else
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		int_arg = -1;
+		return;
+	}
+	pop_stack(head, line_number);
+	pop_stack(head, line_number);
+	int_arg = res;
+	push_stack(head, line_number);
+}
---
+++ mul.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+
+/**
+ * multiply - Multiply top 2 stack elements
+ * @head: double pointer to stack head node
+ * @line_number: line number of instruction in the file
+ *
+ * Return: no return value (void)
+ */
+void multiply(stack_t **head, unsigned int line_number)
+{
+	stack_t *current = *head;
+	int res = 0, count = 0;
+
+	while (current != NULL)
+	{
+		count++;
+		current = current->next;
+	}
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n",
+			line_number);
+		int_arg = -1;
+		return;
+	}
+	current = *head;
+	res = current->n * current->next->n;
+	pop_stack(head, line_number);
+	pop_stack(head, line_number);
+	int_arg = res;
+	push_stack(head, line_number);
+}
