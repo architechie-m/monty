c4016aae5e90d79cfeb77365cb39b21098e5c7b9
EugeneJoe <jmuthui70@gmail.com>
2021-05-12 11:49:26 +0000

    Does nothing

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @head: pointer to head node of stack
+ * @line_number: line number
+ *
+ * Return: no return value (void)
+ */
+void nop(stack_t **head, unsigned int line_number)
+{
+	(void)head;
+	(void)line_number;
+}
