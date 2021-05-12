1b3ce9a1ad050d63548b2da2b835ed8b0972c883
EugeneJoe <jmuthui70@gmail.com>
2021-05-12 11:31:01 +0000

    remove exit function

--- addnode.c
+++ addnode.c
@@ -1,11 +1,18 @@
 #include "monty.h"
 
+/**
+ * add_node - adds the top two stack elements
+ * @head: double pointer to head of stack
+ * @line_number: line number of command being run in the file
+ *
+ * Return: no return value void
+ */
 void add_node(stack_t **head, unsigned int line_number)
 {
 	stack_t *current = *head;
 	int count = 0, sum = 0;
 
-	while(current != NULL)
+	while (current != NULL)
 	{
 		current = current->next;
 		count++;
@@ -13,7 +20,8 @@ void add_node(stack_t **head, unsigned int line_number)
 	if (count < 2)
 	{
 		printf("L%d: can't add, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
+		ourdata.line_number = INT_MAX;
+		return;
 	}
 	current = *head;
 	sum = current->n + current->next->n;
