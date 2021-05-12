83b761fcdd2e48ab052c7a72bd92331cbf2c82a4
EugeneJoe <jmuthui70@gmail.com>
2021-05-12 11:38:53 +0000

    Remove exit function

--- pall.c
+++ pall.c
@@ -2,7 +2,8 @@
 
 /**
  * print_dlistint - prints all elements in a list
- * @h: pointer to the head node
+ * @h: double pointer to the head node
+ * @line_number: line number of instruction in the file
  *
  * Return: returns the number of elements in a node
  */
@@ -10,14 +11,11 @@
 void print_dlistint(stack_t **h, unsigned int line_number)
 {
 	stack_t *current = *h;
-	size_t count = 0;
 	(void)line_number;
 
-	printf("Printing list...\n");
 	while (current != NULL)
 	{
 		printf("%d\n", current->n);
 		current = current->next;
-		count++;
 	}
 }
--- pint.c
+++ pint.c
@@ -1,11 +1,19 @@
 #include "monty.h"
 
+/**
+ * pint - prints the element at the top of the stack
+ * @head: double pointer to the head node of the stack
+ * @line_number: line number of instruction in the file being read
+ *
+ * Return: no return value (void)
+ */
 void pint(stack_t **head, unsigned int line_number)
 {
 	if (head == NULL || *head == NULL)
 	{
-		printf("L%d: can't pint, stack empty", line_number);
-		exit(EXIT_FAILURE);
+		printf("L%d: can't pint, stack empty\n", line_number);
+		ourdata.line_number = INT_MAX;
+		return;
 	}
 	printf("%d\n", (*head)->n);
 }
