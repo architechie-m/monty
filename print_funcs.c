08d4b8b4b1a10c839351b2eabf1c38407670dccc
EugeneJoe <jmuthui70@gmail.com>
2021-05-12 11:39:48 +0000

    Restructured

--- print_funcs.c
+++ print_funcs.c
@@ -1,23 +1,23 @@
 #include "monty.h"
 
 /**
- * print_dlistint - prints all elements in a list
- * @h: pointer to the head node
+ * print_funcst - prints all elements in a list
+ * @h: double pointer to the head node
+ * @line_number: line number of instruction in the file being read
  *
- * Return: returns the number of elements in a node
+ * Return: no return value (void)
  */
-
 void print_funcst(funcs_t **h, unsigned int line_number)
 {
-        funcs_t *current = *h;
-        size_t count = 0;
-        (void)line_number;
+	funcs_t *current = *h;
+	size_t count = 0;
+	(void)line_number;
 
-        printf("Printing list...\n");
-        while (current != NULL)
-        {
-                printf("%s\n", current->n->opcode);
-                current = current->next;
-                count++;
-        }
+	printf("Printing list...\n");
+	while (current != NULL)
+	{
+		printf("%s\n", current->n->opcode);
+		current = current->next;
+		count++;
+	}
 }
