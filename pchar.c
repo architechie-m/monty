678ba9359193e9a36d340e5560ea1e6f5af9dd7b
EugeneJoe <jmuthui70@gmail.com>
2021-05-13 09:07:26 +0000

    Change upper ascii limit to 127

--- pchar.c
+++ pchar.c
@@ -16,7 +16,7 @@ void pchar_stack(stack_t **head, unsigned int line_number)
 		int_arg = -1;
 		return;
 	}
-	if ((*head)->n < 0 || (*head)->n > 177)
+	if ((*head)->n < 0 || (*head)->n > 127)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n",
 			line_number);
