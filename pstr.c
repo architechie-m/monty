0123232dcaa0ae3b5c733db2cbc989270e98684a
EugeneJoe <jmuthui70@gmail.com>
2021-05-13 09:39:57 +0000

    Rework if conditions

--- pstr.c
+++ pstr.c
@@ -12,17 +12,31 @@ void pstr_stack(stack_t **head, unsigned int line_number)
 	stack_t *temp = *head;
 	(void)line_number;
 
-	if (temp == NULL)
+	if (head == NULL || (*head) == NULL)
 	{
 		printf("\n");
 		return;
 	}
 	while (temp)
 	{
-		if (temp->n > 0 && temp->n < 128)
+		if (temp->n == 0)
+		{
+			int_arg = 0;
+			return;
+		}
+		else if (temp->n <= 127 && temp->n >= 1)
+		{
 			printf("%c", temp->n);
+			if (temp->next->n == 0)
+				printf("\n");
+		}
 		else
-			break;
+		{
+			fprintf(stderr, "L%d: can't pchar, value out of range\n"
+				, line_number);
+			int_arg = -1;
+			return;
+		}
 		temp = temp->next;
 	}
 	printf("\n");
